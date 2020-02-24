d71e4214022e34528d076cd78a2bd02fb0e89d5a
Saggitarie <saggitarie@gmail.com>
2020-02-24 14:49:00 +0900

    Made changes on feature branch

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hi there/i);
+  const linkElement = getByText(/I was change on feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
